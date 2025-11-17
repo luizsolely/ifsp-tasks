100d2ac5009a8d040982af479de73db3b8fc090b
luizsolely <158594648+luizsolely@users.noreply.github.com>
2025-11-17 19:39:17 -0300

    Add files via upload

---
+++ EDD2/Proj_Filas_Acessos/Proj_Filas_Acessos/Usuario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj_Filas_Acessos
+{
+    internal class Usuario
+    {
+        private int id;
+        private string nome;
+        private List<Ambiente> ambientes = new List<Ambiente>();
+        public int Id { get => id; set => id = value; }
+        public string Nome { get => nome; set => nome = value; }
+        internal List<Ambiente> Ambientes { get => ambientes; set => ambientes = value; }
+        public Usuario(int id, string name)
+        {
+            this.Id = id;
+            this.Nome = name;
+        }
+        public Usuario() : this(-1,"") { }
+        public Usuario(int id) : this(id, "") { }
+        public bool concederPermissao(Ambiente ambiente)
+        {
+            if (!ambientes.Any(a => a.Id == ambiente.Id))
+            {
+                ambientes.Add(ambiente);
+                return true;
+            }
+            return false;
+        }
+        public bool revogarPermissao(Ambiente ambiente)
+        {
+            return Ambientes.RemoveAll(a => a.Id == ambiente.Id) > 0;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is Usuario u)
+                return this.Id == u.Id;
+            return false;
+        }
+    }
+}
